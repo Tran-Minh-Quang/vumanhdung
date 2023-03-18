6ce82b85e85dedf9e80277b9fad41acb862cd3db
Manh Dung <vinhdunghuy@gmail.com>
2023-03-18 16:54:05 +0700

    .

---
+++ Data/DataContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Team12EUP.Data
+{
+    public class DataContext:DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+    }
+}
