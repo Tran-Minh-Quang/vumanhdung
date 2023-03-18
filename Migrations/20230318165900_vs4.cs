260aa697b3752a0e6c9d3aefd91b7338cc9f9be2
Manh Dung <vinhdunghuy@gmail.com>
2023-03-19 00:00:23 +0700

    .

---
+++ Migrations/20230318165900_vs4.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Team12EUP.Migrations
+{
+    public partial class vs4 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<float>(
+                name: "Mark",
+                table: "historyTests",
+                type: "real",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Mark",
+                table: "historyTests",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(float),
+                oldType: "real");
+        }
+    }
+}
